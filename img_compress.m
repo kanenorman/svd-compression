bd3dbbf1b430390bb74d3dbd42ee07923854b93b
kanenorman <51185594+kanenorman@users.noreply.github.com>
2019-05-30 22:27:42 -0500

    Update img_compress.m

--- img_compress.m
+++ img_compress.m
@@ -8,8 +8,9 @@ A=imread('image.jpg'); % modify accordingly
 X = double(rgb2gray(A));
 X0=X; % save the original image
 
-[Z,ratio]=svdcompress(X0,b,p); %choose a b and p value. 
+[Z,ratio]=svdcompress(X0,b,p); %choose b and p values. 
+
 figure
 colormap('gray');
-   label=sprintf('SVD compression: ratio=%8.4f,error=%8.4f',ratio,norm(Z-X0,'fro')/norm(X0,'fro'));
-   imagesc(Z); title(label);
+label=sprintf('SVD compression: ratio=%8.4f,error=%8.4f',ratio,norm(Z-X0,'fro')/norm(X0,'fro'));
+imagesc(Z); title(label);
